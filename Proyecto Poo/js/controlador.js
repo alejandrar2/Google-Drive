2e13853ddef3e8d0304a585f2c2c9c4b2e2a4c4b
francisGonzalesr <40775111+francisGonzalesr@users.noreply.github.com>
2019-04-05 16:12:42 -0600

    Funcion de botones

--- Proyecto Poo/js/controlador.js
+++ Proyecto Poo/js/controlador.js
@@ -2,3 +2,22 @@ $("#btn-crear").click(function () {
 	// body...
 	alert("Funciona");
 });
+
+$("#btn-sig").click(function () {
+	// body...
+	alert("Funciona");
+	var nombre = $("#nombre").val();
+	var apellido = $("#apellido").val();
+	var correo = $("#correo").val();
+	var contra1 = $("#contra1").val();
+	var contra2 = $("#contra2").val();
+
+	alert(nombre);
+	alert(apellido);
+	alert(correo);
+	alert(contra1);
+	alert(contra2);
+
+	
+
+});
