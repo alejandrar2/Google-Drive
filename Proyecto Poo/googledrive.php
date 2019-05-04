e67bd5fce8972a6b00226c49e2ab6307ff29ae27
Caballero772 <47470117+Caballero772@users.noreply.github.com>
2019-05-03 22:43:36 -0600

    Update googledrive.php

--- Proyecto Poo/googledrive.php
+++ Proyecto Poo/googledrive.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/googledrive.css">
     <link rel="icon" href="img/Google_Drive_logo.png">
     prueba para ver si te aparece el cambio
